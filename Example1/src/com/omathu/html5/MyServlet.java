d6d58f850c3b488845ac1741134b8e1ab6392706
Ganesh <ganmath@gmail.com>
2018-02-09 22:30:52 +0530

    First commit

---
+++ Example1/src/com/omathu/html5/MyServlet.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.omathu.html5;
+
+/**
+ * @author Ganesh
+ *
+ */
+public class MyServlet {
+
+	/**
+	 * 
+	 */
+	public MyServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
